7b09755c4b599dc9b6dd146c2a520d0b09fd6aaa
UlisesSchreiner <schreinerulises@gmail.com>
2023-04-23 02:08:39 -0300

    version 1.0.0

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.holaCursoNPM = exports.holaAlCursoNPM = exports.holaPersonalizado = exports.hola = void 0;
+function hola() {
+    console.log("hola mundo");
+}
+exports.hola = hola;
+function holaPersonalizado(nombre) {
+    console.log("hola", nombre);
+}
+exports.holaPersonalizado = holaPersonalizado;
+function holaAlCursoNPM() {
+    console.log("hola al curso npm");
+}
+exports.holaAlCursoNPM = holaAlCursoNPM;
+function holaCursoNPM() {
+    console.log("hola npm");
+}
+exports.holaCursoNPM = holaCursoNPM;
---
+++ lib/index.ts
@@ -0,0 +1,15 @@
+  export function hola () {
+    console.log("hola mundo");
+  }
+
+  export function holaPersonalizado (nombre: string) {
+    console.log("hola", nombre);
+  }
+
+  export function holaAlCursoNPM () {
+    console.log("hola al curso npm");
+  }
+
+  export function holaCursoNPM() {
+    console.log("hola npm")
+  }
